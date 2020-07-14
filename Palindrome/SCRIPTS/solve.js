c62fda0b80e0cb8f274aa89ae45df2e427e7f969
Beth Olmo <betholmo13@gmail.com>
2020-07-13 23:39:56 -0400

    Prism placement & styling

---
+++ Palindrome/SCRIPTS/solve.js
@@ -0,0 +1,34 @@
+﻿$("#submit").click(function () {
+        let word = $("#userWord").val()
+            $("#msg1").text(`You entered: ${word}`);
+
+            if (word.length < 2) {
+        alert("Please use at least 2 letters")
+    } else {
+        let cleanWord = word.toLowerCase().replace(/\s|\'|\"/g, '');
+                $("#msg2").text(`Cleaned up a bit, that's: ${cleanWord}.`);
+
+                let revWord = word.split('').reverse().join('');
+                $("#msg3").text(`Reversed, it's: ${revWord}.`);
+
+                let cleanRevWord = revWord.toLowerCase().replace(/\s|\'|\"/g, '');
+                $("#msg4").text(`Now when we clean it up, it's ${cleanRevWord}.`);
+
+                let result = "";
+
+                if (cleanWord == cleanRevWord) {
+        result = `So, ${word} IS a palindrome!`;
+                } else {
+        result = `So, ${word} is NOT a palindrome.`;
+                };
+
+                $("#finalMsg").text(" " + result);
+            }
+        });
+$("#clear").click(function () {$("#userWord").val("");
+$("#msg1").text("");
+$("#msg2").text("");
+$("#msg3").text("");
+$("#msg4").text("");
+$("#finalMsg").text("");
+});
